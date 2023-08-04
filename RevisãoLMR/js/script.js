55b4cbd3c8c8f34c1658b2b4b2d9fe461f2a1416
alberto12345678900 <albertomeloteco@gmail.com>
2023-08-04 16:28:04 -0300

    04/08/2023

---
+++ RevisãoLMR/js/script.js
@@ -0,0 +1,8 @@
+function validacao() {
+    let nome = document.getElementById("validationCustom01").value;
+    
+    if (nome=="") {
+        alert("Nome Vazio!!!!")
+        return false;
+    }
+}
