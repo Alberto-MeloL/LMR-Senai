6f42ae17fc3442d360c6c2feec6600b0cf230781
alberto12345678900 <125086588+alberto12345678900@users.noreply.github.com>
2023-08-31 12:47:27 +0000

    ok

--- aulaJS/js/main.js
+++ aulaJS/js/main.js
@@ -1,5 +1,5 @@
-var nome = window.prompt('Qual é o seu nome?')
 function newName() {
+    var nome = window.prompt('Qual é o seu nome?')
     currentName = window.document.getElementById('footerName')
     currentName.innerText += `Olá ${nome}`
 }
