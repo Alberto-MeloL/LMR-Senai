a294b9e7b7cb59f1298891ce9d58dd4f435007c9
Alberto-MeloL <albertomeloteco@gmail.com>
2023-09-25 16:56:54 -0300

    ok

--- cadastroHerarquia/js/main.js
+++ cadastroHerarquia/js/main.js
@@ -4,3 +4,25 @@ const navBar = document.querySelector("#nav-bar");
 menuToggle.addEventListener("click", () => {
     navBar.classList.toggle("menu-open");
 });
+
+var opcoes = window.document.getElementsByClassName(opcoesForm);
+var motivo = window.document.querySelector(mtv);
+var nome = window.document.querySelector(nomeCadastro);
+var email = window.document.querySelector(emailCadastro);
+var senha = window.document.querySelector(senhaCadastro);
+
+//validações de cadastro e login
+
+//Verificando se há campos vazios
+if (nome.value.length == 0) {
+    alert('Por favor preencha o campo "nome"')
+}else if (email.value.length == 0) {
+    alert('Por favor preencha o campo "email"')
+}else if (senha.value.length == 0) {
+    alert('Por favor preencha o campo "senha"')
+}//vericando se os campos estao preenchidos corretamente
+else if (nome.value.length == Number) {
+    alert('O campo "nome" não pode conter números!')
+}else if (email.value. == -1'@' ) {
+    
+}
