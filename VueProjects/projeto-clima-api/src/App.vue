754e6fd77e3e59d0c1da066b5762cb39c72961df
Alberto-MeloL <albertomeloteco@gmail.com>
2023-11-17 16:31:39 -0300

    17/11/2023

---
+++ VueProjects/projeto-clima-api/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+  <Clima />
+  </div>
+  </template>
+  <script>
+  import Clima from './components/Clima.vue';
+  export default {
+  name: 'App',
+  
+  components: {
+  Clima,
+  },
+  };
+  </script>
